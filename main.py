fbaad393735c50e4f02a6769a5c6c032169ca283
Rhum2notTheOne <romspeed92@icloud.com>
2025-01-05 04:29:23 +0100

    minors adjustements

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import epreuves_mathematiques
 import enigme_pere_fouras
 import epreuve_finale
 import random
-from fonctions_utiles import clearscreen
+from fonctions_utiles import introduction, clearscreen
 
 
-def main():
+def jeu():
+    introduction()
     clees = 0
     epreuves = {
         "epreuves_hasard": epreuves_hasard,
@@ -28,15 +29,19 @@ def main():
                 raise ValueError
             ep = list(epreuves.values())[choice - 1]
             if ep.pick():
+                #cette écriture est permise par le polymorphisme ;
+                # si ep.pick retourne vrai alors
+                # c'est que le joueur à réussi l'epreuve choisie
                 clees += 1
             # un choix en dehors des limites va être considéré comme une ValueError
         except TypeError or ValueError:
             print(f"Veuillez entrer un nombre entre 1 et {len(epreuves.keys())}")
             continue
-    epreuve_finale.salle_du_tresor(
-    # epreuve finale, le moment ou l'on
+
+    epreuve_finale.salle_du_tresor()
+    # epreuve finale, le moment ou l'on doit deviner le mot clé
 
 
 
 if __name__ == "__main__":
-    main()
+    jeu()
